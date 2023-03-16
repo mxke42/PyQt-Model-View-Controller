85c7d6b6564577858bb4b615b77fa41c877fdf4a
mxke42 <mkeirn@gmail.com>
2023-03-16 15:31:00 -0600

    Update model.py

--- model/model.py
+++ model/model.py
@@ -2,12 +2,11 @@ from PyQt5.QtCore import QObject, pyqtSignal, QAbstractListModel
 #import PyQt5
 
 
-
 class Model(QObject):
+    resultsStrChanged= pyqtSignal(str)
+    websitesChanged = pyqtSignal(list)
     def __init__(self):
         super().__init__()
-        # self._websiteList = None
-        # self._resultsStr = None
 
     @property
     def websiteList(self):
@@ -20,10 +19,13 @@ class Model(QObject):
     @websiteList.setter
     def websiteList(self, value):
         self._websiteList = value
+        self.websitesChanged.emit(value)
 
     @resultsStr.setter
     def resultsStr(self, value):
         self._resultsStr = value
+        self.resultsStrChanged.emit(value)
+
 
 
 
